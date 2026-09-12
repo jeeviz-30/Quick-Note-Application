0d79e3d6b0c2c8d2cf0ef4714e952c64522db13b
jeeviz-30 <YourGitHubEmail>
2026-09-13 01:36:38 +0530

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const path = path = require('path');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
